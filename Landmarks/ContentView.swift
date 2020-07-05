25e45bf2a6a9222a7c131bca26a61bb093ce7402
nitaking <10850034+nitaking@users.noreply.github.com>
2020-07-05 13:26:12 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by user2 on 2020/07/05.
+//  Copyright © 2020 nitaking. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
